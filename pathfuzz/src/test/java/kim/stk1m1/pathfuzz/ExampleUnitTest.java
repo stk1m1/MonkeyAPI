d4e19fc51974f41273dec9cbee3f5204c4c94e40
SeongTaekC <stk@realm.io>
2016-06-19 00:15:54 +0900

    first build

---
+++ pathfuzz/src/test/java/kim/stk1m1/pathfuzz/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package kim.stk1m1.pathfuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
